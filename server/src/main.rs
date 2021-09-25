79846845addd32ad5741c47cfdb9e9fb66fa6618
MoDPhoenix <ivanchenko.b@gmail.com>
2021-09-25 03:01:12 +0300

    feat: add settings for server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,13 @@
-mod domain;
+use crate::settings::get_settings;
 
-fn main() {
+mod domain;
+mod settings;
+
+fn main() -> std::io::Result<()> {
+    let settings = get_settings().expect("Failed to read settings.");
+
     println!("Hello, world! This is the pokerplanning.org");
+    println!("Server address: {}", settings.get_server_address());
+
+    Ok(())
 }
---
+++ server/src/settings.rs
@@ -0,0 +1,30 @@
+use config::ConfigError;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Settings {
+    pub host: String,
+    pub port: u16,
+}
+
+pub fn get_settings() -> Result<Settings, ConfigError> {
+    // Initialize our settings reader
+    let mut settings = config::Config::default();
+
+    // Add configuration values from a file named `settings`.
+    // It will look for any top-level file with an extension
+    // that `config` knows how to parse: yaml, json, etc.
+    settings.merge(config::File::with_name("settings"))?;
+
+    // Try to convert the configuration values it read into
+    // our Settings type
+    settings.try_into()
+}
+
+// let address = format!("127.0.0.1:{}", configuration.application_port);
+
+impl Settings {
+    pub fn get_server_address(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
+}
