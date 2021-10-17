92051d3d7f16a760eac4b1e2fdd7231e646cfc25
MoDPhoenix <ivanchenko.b@gmail.com>
2021-10-17 23:45:48 +0300

    feat: fix and improve NotFoundPage

--- client/src/pages/NotFoundPage/index.tsx
+++ client/src/pages/NotFoundPage/index.tsx
@@ -1,18 +1,30 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+
+import { PageLayout } from 'components';
 
 export function NotFoundPage(): ReactElement {
-
   return (
-        <Box
-	      sx={{
-	        display: 'flex',
-	        flexDirection: 'column',
-	        alignItems: 'center',
-	      }}
-	    >
-      		<Typography component="h1">Page Not Found</Typography>
-	    	</Box>
-      );
+    <PageLayout>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          flexGrow: 1,
+        }}
+      >
+        <Typography component="h1" variant="h3">
+          Page Not Found
+        </Typography>
+        <Link to="/">
+          <Typography component="span" variant="body1">
+            Go to home
+          </Typography>
+        </Link>
+      </Box>
+    </PageLayout>
+  );
 }
