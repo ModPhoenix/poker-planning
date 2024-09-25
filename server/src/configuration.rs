de840ed4e16492482e3144574ff926396944a99e
modphoenix <ivanchenko.b@gmail.com>
2024-09-25 22:35:13 +0300

    chore: remove unused prop form ApplicationSettings

--- server/src/configuration.rs
+++ server/src/configuration.rs
@@ -13,7 +13,6 @@ pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
-    pub base_url: String,
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
