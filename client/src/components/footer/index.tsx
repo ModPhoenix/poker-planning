6e66a5a7e2fe7a390362c96efaa116e2efe3825c
modphoenix <ivanchenko.b@gmail.com>
2024-09-27 00:44:37 +0300

    fix: footer styles

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -29,25 +29,28 @@ const navigation = {
 
 export const Footer: FC = () => {
   return (
-    <footer aria-labelledby="footer-heading" className="bg-gray-900">
+    <footer
+      aria-labelledby="footer-heading"
+      className="bg-white dark:bg-gray-900"
+    >
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-32">
-        <div className="mt-16 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-8 lg:px-8">
+        <div className="md:flex md:items-center md:justify-between">
           <div className="flex space-x-6 md:order-2">
             {navigation.social.map((item) => (
               <a
                 key={item.name}
                 href={item.href}
-                className="text-gray-500 hover:text-gray-400"
+                className="text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400"
               >
                 <span className="sr-only">{item.name}</span>
                 <item.icon aria-hidden="true" className="h-6 w-6" />
               </a>
             ))}
           </div>
-          <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
+          <p className="mt-8 text-xs leading-5 text-gray-500 dark:text-gray-400 md:order-1 md:mt-0">
             PokerPlanning.org
           </p>
         </div>
